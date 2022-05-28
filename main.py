af892c125e451a9c04dc1fb554849d999daa1807
nonpirat <pirat.pot@gmail.com>
2022-05-28 15:18:46 +0700

    Add while loop for command input

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import serial
 import time
+import RPi.GPIO as GPIO
 
 
 class GripperIAI:
-    def __init__(self,port='/dev/ttyUSB0',baudrate=38400,timeout=3):
+    def __init__(self,port='/dev/ttyUSB0',baudrate=38400,timeout=3,relay_pin=18, target_location=(50,50,50), desired_location=(0,0,0)):
         
         # Set the port and baudrate for serial communication
         self.port = port
         self.baudrate = baudrate
+        
         self.timeout = timeout
         
         self.ser = serial.Serial(self.port, self.baudrate, timeout= self.timeout)
+        
+        # Set up the GPIO for relay
+        self.relay_pin = relay_pin
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.relay_pin,GPIO.OUT)
         
         # Turn on the servo
         self.activate()
         
         #Set Home
         self.set_home()
+        
+        # Set target and desired location
+        self.target_location = target_location
+        self.desired_location = desired_location
     
     def activate(self):
         
@@ -35,14 +47,20 @@ class GripperIAI:
         self.ser.write('!0023307000000a0\r\n'.encode())
         read = self.ser.readline().decode()
         print(read+'\n')
+        read = self.ser.readline().decode()
+        print("Resetting Position")
+        print(read+'\n')
         
         # Wait for the robot to finish
         time.sleep(5)
 
 
 
-    def move(self,message_id,axis,acceleration,speed,x_target,y_target,z_target) :
-
+    def move(self,message_id='absolute',axis='xyz',acceleration=1.0,speed=50,x_target=0,y_target=0,z_target=0) :
+        
+        target_speed = speed
+        moving = message_id
+        
         if message_id == 'absolute' :
             message_id = '234'
         elif  message_id == 'relative' :
@@ -120,10 +138,66 @@ class GripperIAI:
         string_command = string_command + checksum
 
         self.ser.write((string_command +'\r\n').encode())
+        print('Moving {}ly to x: {}, y: {}, z: {} with speed: {}'.format(moving, x_target, y_target, z_target, target_speed))
         read = self.ser.readline().decode()
-        print(read+'\n')
+        if (read[0]=='#'):
+            print("Success")
+        else:
+            print("Error")
+            print(read+'\n')
+        time.sleep(3)
+        
+    def close_gripper(self):
+        
+        GPIO.output(self.relay_pin,1)
+        time.sleep(2)
+        
+    def open_gripper(self):
+        
+        GPIO.output(self.relay_pin,0)
+        time.sleep(2)
+        
+    def move_and_grip(self):
+        
+        #Gripper open
+        self.open_gripper()
+        
+        # Move to target location
+        self.move(x_target=self.target_location[0])
+        self.move(x_target=self.target_location[0],y_target=self.target_location[1])
+        self.move(x_target=self.target_location[0],y_target=self.target_location[1],z_target=self.target_location[2])
+        
+        #Gripper close
+        self.close_gripper()
+        
+        
+        # Move to desired Location
+        self.move(x_target=self.target_location[0],y_target=self.target_location[1])
+        self.move(x_target=self.target_location[0],y_target=self.desired_location[1])
+        self.move(x_target=self.desired_location[0],y_target=self.desired_location[1])
+        self.move(x_target=self.desired_location[0],y_target=self.desired_location[1],z_target=self.desired_location[2])
+        
+        # Open Gripper
+        self.open_gripper()
+        
+    def __call__(self):
+        
+        while True:
+            
+            ans = str(input('Object Placed?: '))
+            if(ans.lower()=='y' or ans.lower()=='yes'):
+                self.move_and_grip()
+            elif(ans.lower()=='quit' or ans.lower()=='exit'):
+                break
+            else:
+                continue
+                
+        
     
-gripper = GripperIAI()
+gripper = GripperIAI(target_location=(60,100,75),desired_location=(0,0,75))
+gripper()
+
+#gripper.move('absolute','xyz',acceleration=1.0,speed=50,x_target=200,y_target=200,z_target=100)
 
 
     
